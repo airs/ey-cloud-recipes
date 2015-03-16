6e1a10f1728f0bf3a75d9cb5e982d6e79472cac4
Masakuni Kato <kato@airs.co.jp>
2015-03-16 23:25:01 +0900

    update ipa-gothic

--- cookbooks/ipa-gothic/recipes/default.rb
+++ cookbooks/ipa-gothic/recipes/default.rb
@@ -3,7 +3,12 @@
 # Recipe:: default
 #
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
-  remote_file "/usr/share/fonts/ipa-gothic/ipag.ttf" do
+  directory "/usr/share/fonts/ipa-gothic" do
+    mode 00755
+    action :create
+  end
+
+  cookbook_file "/usr/share/fonts/ipa-gothic/ipag.ttf" do
     owner 'root'
     group 'root'
     mode 0644
