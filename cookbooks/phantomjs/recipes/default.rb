4cbaf01ca3fde22acffe5184d4a79d0624e33c7a
Masakuni Kato <kato@airs.co.jp>
2015-03-16 23:35:20 +0900

    change from deprecated remote_file to cookbook_file instead

--- cookbooks/phantomjs/recipes/default.rb
+++ cookbooks/phantomjs/recipes/default.rb
@@ -3,7 +3,7 @@
 # Recipe:: default
 #
 if ['solo', 'app', 'app_master'].include?(node[:instance_role])
-  remote_file "/usr/local/bin/phantomjs" do
+  cookbook_file "/usr/local/bin/phantomjs" do
     owner node[:owner_name]
     group node[:owner_name]
     mode 0755
