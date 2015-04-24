e587a51cea9120757daa0feeebc4a7a6f52b24da
Masakuni Kato <kato@airs.co.jp>
2015-04-24 23:13:50 +0900

    add message by whenever recipe

--- cookbooks/whenever/recipes/default.rb
+++ cookbooks/whenever/recipes/default.rb
@@ -6,4 +6,8 @@ if node[:instance_role] == 'app_master'
   execute "update-crontab" do
     command '/usr/local/bin/bundle exec whenever --update-crontab'
   end
+
+  ey_cloud_report "update-crontab" do
+    message "Update crontab by whenever"
+  end
 end
