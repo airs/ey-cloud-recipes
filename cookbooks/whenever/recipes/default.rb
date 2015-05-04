e0f70b4d176fee0deff6323890cd21fefdbcb440
Masakuni Kato <kato@airs.co.jp>
2015-05-05 00:44:35 +0900

    Disable whenever recipe

--- cookbooks/whenever/recipes/default.rb
+++ cookbooks/whenever/recipes/default.rb
@@ -4,8 +4,8 @@
 #
 if node[:instance_role] == 'app_master'
   execute "update-crontab" do
-    user 'deploy'
-    cwd '/data/amaten_production/current'
-    command '/usr/local/bin/bundle exec whenever --update-crontab'
+    # user 'deploy'
+    # cwd '/data/amaten_production/current'
+    # command '/usr/local/bin/bundle exec whenever --update-crontab'
   end
 end
